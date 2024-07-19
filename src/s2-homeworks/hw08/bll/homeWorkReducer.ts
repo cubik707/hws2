fb499db2f267ac8228173003f59a49cc0c248dfc
Vladislava <stdemidovez@gmail.com>
2024-07-19 22:36:17 +0300

    hw-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return action.payload === "up"
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : [...state].sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age > 18) // need to fix
         }
         default:
             return state
